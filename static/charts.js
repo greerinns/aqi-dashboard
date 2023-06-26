9cf9ab1f46d45b99e616253a46cd5f1e66352527
Katie Djahan <katiedjahan@yahoo.com>
2023-06-25 21:31:34 -0700

    bar chart edits to get avg aqi, not done

--- static/charts.js
+++ static/charts.js
@@ -1,13 +1,120 @@
 // Add js logic here
 function init(response) {
-// ------------------------- BAR CHART FOR STATE AQIs -----------------------------------------
-// later on would be good to sort / color code / make the value the average instead of total
-  // Sorting data by AQI severity
-  // Trace for the state and AQI data
+// ------------------------- BAR CHART FOR STATE AQIs -------------------------
+// Trace for the state and AQI data
+//   let traceJanState = {
+//     x: response.map(object => object.state_id),
+//     y: response.map(object => object.AQI),
+//     type: "bar"
+//   };
+
+//   // Data trace array
+//   let dataJanState = [traceJanState];
+
+//   // Apply the group barmode to the layout
+//   let layoutJanState = {
+//     title: "State AQI Results"
+//   };
+
+//   // Render the plot to the div tag with id "plot"
+//   Plotly.newPlot("bar", dataJanState, layoutJanState);
+
+// ------------------------- BAR CHART FOR STATE AQIs -------------------------
+  createBar(response);
+// ------------------------- MAP FOR AQIs -------------------------
+// Using create map function made outside of the init function
+  createMap(response);
+// ------------------------- CHOROPLETH MAP FOR AQIs -------------------------
+  createChloro(response);
+// ------------------------- BUBBLE CHART FOR AQIs -------------------------
+  createBubble(response);
+// ------------------------- PIE CHART FOR AQIs -------------------------
+  createPie(response);
+};
+
+// ########################################### 
+// BAR FUNCTION CREATION 
+// ###########################################
+// Trace for the state and AQI data
+function createBar(response) {
+
+  let states = response.map(object => object.state_id)
+    .filter((value, index, self) => self.indexOf(value) === index);
+  // console.log(states);
+  let aqi = "AQI";
+
+  function mean(response, states, aqi) {
+    let count = 0;
+    let total = 0;
+    for (let i = 0; i < response.length; i++) {
+      row = response[i];
+
+      if (row.state_id == states){
+        total += row[aqi];
+        count += 1;
+      }
+    }
+    let meanAQI = total / count;
+
+    return meanAQI;
+  }
+  // let state = states.map(object => object);
+  // console.log(state);
+  // let meanAQIData = mean(response, "CA", aqi);
+  // console.log(meanAQIData);
+
+  function xValue (){
+    for (let i = 0; i < states.length; i++) {
+    let meanAQIData = mean(response, states[i], aqi);
+    console.log(meanAQIData);
+  };
+}
+  // console.log(meanAQIData);
+
+  // for (let i = 0; i < states.length; i++) {
+  //   let meanAQIFunc = mean(response, states[i], aqi);
+  //   console.log(meanAQIFunc);
+
+  //   let traceJanState = {
+  //     x: meanAQIFunc,
+  //     y: states[i],
+  //     type: "bar",
+  //     orientation: "h"
+  //   };
+
+  //   // Data trace array
+  // let dataJanState = [traceJanState];
+
+  // // Apply the group barmode to the layout
+  // let layoutJanState = {
+  //   title: "State AQI Results"
+  // };
+
+  // // Render the plot to the div tag with id "plot"
+  // Plotly.newPlot("bar", dataJanState, layoutJanState);
+  // };
+
+  // const groupByStateID = response.map(object => object).groupByToMap(object => object.state_id);
+  // console.log(groupByStateID);
+  // console.log(response); --- this is all the data for January (by month)
+  // let good = response.map(object => object.Category).filter(catGood);
+
+  let aqiData = response.map(object => object)
+  let meanAQI = xValue();
+  console.log(meanAQI);
+  // let sortedByAQI = response.map(object => object).sort((a, b) => b.AQI - a.AQI);
+  // let sortedByAQI = meanAQI.sort((a, b) => b.AQI - a.AQI);
+
+  // slicedData = sortedByAQI.slice(0, 10);
+
+  // reversedData = slicedData.reverse();
+  // reversedData = meanAQI.reverse();
+
   let traceJanState = {
-    x: response.map(object => object.state_id),
-    y: response.map(object => object.AQI),
-    type: "bar"
+    x: meanAQI,
+    y: states.map(object => object),
+    type: "bar",
+    orientation: "h"
   };
 
   // Data trace array
@@ -21,24 +128,30 @@ function init(response) {
   // Render the plot to the div tag with id "plot"
   Plotly.newPlot("bar", dataJanState, layoutJanState);
 
-// ------------------------- MAP FOR AQIs -----------------------------------------
-// Using create map function made outside of the init function
-  createMap(response);
-// ------------------------- CHLOROPLETH MAP FOR AQIs -----------------------------------------
-  createChloro(response);
-// ------------------------- BUBBLE CHART FOR AQIs -----------------------------------------
-  createBubble(response);
-// ------------------------- PIE CHART FOR AQIs -----------------------------------------
-  createPie(response);
+  // ----------------------- ORIGINAL BAR CHART LAYOUT -----------------------
+  //   // Trace for the state and AQI data
+  // let traceJanState = {
+  //   x: response.map(object => object.state_id),
+  //   y: response.map(object => object.AQI),
+  //   type: "bar"
+  // };
+
+  // // Data trace array
+  // let dataJanState = [traceJanState];
+
+  // // Apply the group barmode to the layout
+  // let layoutJanState = {
+  //   title: "State AQI Results"
+  // };
+
+  // // Render the plot to the div tag with id "plot"
+  // Plotly.newPlot("bar", dataJanState, layoutJanState);
 };
 
-// function initBubble(response) {
-//   // ------------------------- BUBBLE CHART FOR AQIs -----------------------------------------
-//   createBubble(response);
-//   // createPie(response);
-// };
 
-// -------------------------MAP FUNCTION CREATION-----------------------------------------
+// ###########################################
+// MAP FUNCTION CREATION
+// ###########################################
 function createMap(response) {      
   
   var mapScl = [[0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],[0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],[0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']];
@@ -90,12 +203,12 @@ function createMap(response) {
 
       Plotly.newPlot("map", mapData, mapLayout, {showLink: false});
       // source: https://plotly.com/javascript/scatter-plots-on-maps/
-    }
+};
 
-// ------------------------- CHLOROPLETH FUNCTION -----------------------------------------
+// ########################################### 
+// CHOROPLETH FUNCTION 
+// ###########################################
 function createChloro(response) {
-  // console.log(response.map(object => object.AQI))
-  // console.log(response.map(object => object.state_id))
       var dataChloro = [{
           type: 'choropleth',
           locationmode: 'USA-states',
@@ -134,12 +247,12 @@ function createChloro(response) {
       Plotly.newPlot("mapChloro", dataChloro, layoutChloro, {showLink: false});
 
 // source https://plotly.com/javascript/choropleth-maps/
-}
+};
 
-// ------------------------- BUBBLE CHART FUNCTION -----------------------------------------
+// ########################################### 
+// BUBBLE CHART FUNCTION 
+// ###########################################
 function createBubble(response){
-  // const stateEventData = document.getElementById('selDatasetStates');
-  // stateEventData.addEventListener('change', stateChange);
   const bubbleChart = new Chart(
     document.getElementById('bubble'),
     {
@@ -161,9 +274,11 @@ function createBubble(response){
       }
     }
   );
-}
+};
 
-// ------------------------- PIE CHART FUNCTION -----------------------------------------
+// ########################################### 
+// PIE CHART FUNCTION 
+// ###########################################
 function createPie(response) {
   // let dp25 = response.map(object => object.Category)
   // let dp10 = 
@@ -182,10 +297,6 @@ function createPie(response) {
   // catUnhealthy =[]
   // catSensitive =[]
   // catHazardous =[]
-
-  // for (let i = 0; i < response.length; i++) {
-  //   if response.map(object => object.Category) 
-  // }
 
   function catGood(category) {
     return category == "Good";
@@ -208,13 +319,6 @@ function createPie(response) {
   let unhealthy = response.map(object => object.Category).filter(catUnhealthy);
   let sensitive = response.map(object => object.Category).filter(catSensitive);
   let haz = response.map(object => object.Category).filter(catHazardous);
-  // console.log(good.length);
-  // console.log(mod.length);
-  // console.log(unhealthy.length);
-  // console.log(sensitive.length);
-  // console.log(haz.length);
-
-  // good, moderate, unhealthy, Unhealthy for Sensitive Groups, hazardous
 
   const pieChart = new Chart(
     document.getElementById('pie'),
@@ -250,14 +354,17 @@ function createPie(response) {
   )
 };
 
-// ------------------------- DROP DOWN CHANGES -----------------------------------------
-// Call updatePlotly() when a change takes place to the DOM
+// ###########################################
+// DROP DOWN CHANGES 
+// ###########################################
 d3.selectAll("#selDataset").on("change", updateCharts);
 d3.selectAll("#selDatasetStates").on("change", updateBubble);
 
 // when the dropdown list changes then run this, this is a function to change the bar chart
 
+// ###########################################
 // This function is called when a month dropdown menu item is selected
+// ###########################################
 function updateCharts() {
   // Use D3 to select the dropdown menu
   let dropdownMenu = d3.select("#selDataset");
@@ -268,21 +375,26 @@ function updateCharts() {
   let urlBaseMonth = "/api/v1.0/aqi/month/"
   // ---- NEW API CALL----
   d3.json(urlBaseMonth.concat(monthNumber)).then(function(monthData){
-     // ---- UPDATING BAR CHART ----
-    let newX = monthData.map(object => object.state_id);
-    let newY = monthData.map(object => object.AQI);
-    Plotly.restyle("bar", "x", [newX]);
-    Plotly.restyle("bar", "y", [newY]);
+    //  // ---- UPDATING BAR CHART ----
+    // let newX = monthData.map(object => object.state_id);
+    // let newY = monthData.map(object => object.AQI);
+    // Plotly.restyle("bar", "x", [newX]);
+    // Plotly.restyle("bar", "y", [newY]);
     // ---- NEW MAP ----
-    createMap(monthData)
+    createBar(monthData);
+    // ---- NEW MAP ----
+    createMap(monthData);
     // ---- NEW CHLORO ----
     createChloro(monthData)
     // ---- NEW PIE ----
     createPie(monthData);
   });
-}
+};
 
+
+// ###########################################
 // This function is called when a state dropdown menu item is selected
+// ###########################################
 function updateBubble() {
   // Use D3 to select the dropdown menu
   let dropdownMenu = d3.select("#selDatasetStates");
@@ -295,26 +407,22 @@ function updateBubble() {
   d3.json(urlBaseState.concat(stateValue)).then(function(stateData){
     // ---- NEW BUBBLE ----
     createBubble(stateData);
-    // bubbleChart.update(stateData)
-    // ---- NEW MAP ----
-    // createMap(monthData)
     // ---- NEW PIE ----
     createPie(stateData);
   });
-}
+};
+
 
 let urlMonth = "/api/v1.0/aqi/month/"
 d3.json(urlMonth.concat(1)).then(function(response){
-  //console.log(response);
   init(response)
   // checking on data
   // run functions to create dash here
 });
 
+
 let urlState = "/api/v1.0/aqi/state/"
 d3.json(urlState.concat(1)).then(function(response){
-  //console.log(response);
-  // initBubble(response)
   init(response)
   // checking on data
   // run functions to create dash here
